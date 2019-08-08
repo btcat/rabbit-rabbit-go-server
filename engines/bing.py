07520febed838bba2e3ee27c4a8fd2a459fcc280
Xiangrui2019 <xiangrui@aiursoft.com>
2019-08-08 18:53:30 +0800

    r

--- engines/bing.py
+++ engines/bing.py
@@ -1,3 +1,11 @@
 # /usr/bin/python3
 # -*- encoding: utf-8 -*-
 
+class BingEngine():
+    def __init__(self):
+        self.base_url = "https://www.bing.com/"
+
+    def _get_offset_from_pageno(self, pageno):
+        return (pageno - 1) * 10 + 1
+
+    def query(self, keyword="", pageno=1):
