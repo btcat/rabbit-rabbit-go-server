9366249f87116ce43d79f3d780e942a40d99806d
Xiangrui2019 <xiangrui@aiursoft.com>
2019-08-09 11:24:09 +0800

    add merger

--- mergeor/mergeor.py
+++ mergeor/mergeor.py
@@ -1,8 +1,18 @@
 # /usr/bin/python3
 # -*- encoding: utf-8 -*-
 
+from engines.github import GithubEngine
+from engines.stackoverflow import StackoverflowEngine
+
 class Mergeor():
     def __init__(self):
-        pass
+        self.github = GithubEngine()
+        self.stackoverflow = StackoverflowEngine()
 
     def search(self, keyword="", pageno=1):
+        github = self.github.search(keyword, pageno)
+        stackover = self.github.search(keyword, pageno)
+        return {
+            'github': github,
+            'stackoverflow': stackover,
+        }
